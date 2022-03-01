8f8efd3643962666a44fc21006519cc35499089b
Pawel Czernecki <czerneckip1@gmail.com>
2022-03-01 18:40:10 +0100

    add atribute routing in route config

--- BookcaseManager/App_Start/RouteConfig.cs
+++ BookcaseManager/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace BookcaseManager
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
