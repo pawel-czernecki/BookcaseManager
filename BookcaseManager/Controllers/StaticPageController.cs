9bbc2a9fdb7a5ab815cf814b7be53e8810a76ac3
Pawel Czernecki <czerneckip1@gmail.com>
2022-03-01 17:27:50 +0100

    add static page controller with method that returns a homepage

---
+++ BookcaseManager/Controllers/StaticPageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookcaseManager.Controllers
+{
+    public class StaticPageController : Controller
+    {
+        //<summary>
+        //Provides a static pages
+        //</summary>
+
+        // GET: StaticPage
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
