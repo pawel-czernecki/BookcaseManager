6fb0ac9f58acb877c56e45283d7f2f39c3ef4623
Pawel Czernecki <czerneckip1@gmail.com>
2022-03-11 17:31:32 +0100

    add authentication by using formsauthentication in membershiprovider and pages to login and register new user

---
+++ BookcaseManager/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using BookcaseManager.Models;
+
+namespace BookcaseManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Post: Account/Login
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Nickname, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Nickname, true);
+
+                    return RedirectToAction("Home", "StaticPage");
+                }
+                else
+                    ModelState.AddModelError("", "Niepoprawne dane uwierzytelniające.");
+            }
+            return View(model);
+        }
+
+        //GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //POST: Account/Register
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                MembershipCreateStatus createStatus;
+                Membership.CreateUser(model.Nickname, model.Password, model.Email, null, null, true, out createStatus);
+
+                if(createStatus == MembershipCreateStatus.Success)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Nickname, true);
+                    return RedirectToAction("Home", "StaticPage");
+                }
+                else
+                {
+                    ModelState.AddModelError("", createStatus.ToString());
+                }   
+            }
+            return View(model);
+        }
+
+        //GET: Account/Logout
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Home", "StaticPage");
+        }
+    }
+}
---
+++ BookcaseManager/Models/AccountModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookcaseManager.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Nickname { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Nickname { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Hasło musi składać się conajmniej z 6 znaków, a maksymalnie ze 100.")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Podane hasła nie są identyczne.")]
+        public string RetypePassword { get; set; }
+    }
+}
